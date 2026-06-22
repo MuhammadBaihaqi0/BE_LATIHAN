af9e8e9ad41f9d4b0e0c9e2093bf392ee1d08eb1
Mhd Baihaqi Siregar <baihaqisiregar09@gmail.com>
2026-06-22 22:40:49 +0700

    update project

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://belatihan-production-7069.up.railway.app",
+	"https://my-fe-ten.vercel.app",
 }
 
 func GetAllowedOrigins() []string {
